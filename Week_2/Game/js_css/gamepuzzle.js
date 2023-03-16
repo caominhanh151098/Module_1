5f7c91f8e3ca45cb0dde32211e5bf14d00d7e0f6
caominhanh151098 <cao.minhanh151098@gmail.com>
2023-03-16 11:01:34 +0700

    fix Bai10.html

---
+++ Week_2/Game/js_css/gamepuzzle.js
@@ -0,0 +1,14 @@
+document.getElementById("1").addEventListener("click", IMG);
+document.getElementById("2").addEventListener("click", IMG);
+document.getElementById("3").addEventListener("click", IMG);
+document.getElementById("4").addEventListener("click", IMG);
+document.getElementById("5").addEventListener("click", IMG);
+
+function switchIMG(item) {
+    let x = Math.floor(Math.random() * 3);
+    console.log(item);
+
+
+
+
+}
